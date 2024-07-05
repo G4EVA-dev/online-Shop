5507d4bb545dbd173d791d6407a8225080609cf1
G4EVA-dev <tanzeglenn@gmail.com>
2024-07-05 16:09:39 +0100

    set-up broser routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Layout from './components/layout'
+import Home from './pages/home'
+import Detail from './pages/detail'
+import { BrowserRouter, Routes, Route } from 'react-router-dom' ;
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />} >
+          <Route index element={<Home />} />
+          <Route path='/:slug' element={<Detail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/layout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Outlet} from 'react-router-dom'
+import Header from './header'
+import CartTab from './cartTab'
+
+const Layout = () => {
+  return (
+    <div>
+      <main>
+        <Header />
+        <Outlet />
+      </main>
+      <CartTab />
+    </div>
+  )
+}   
+
+export default Layout

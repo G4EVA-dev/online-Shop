a83c459dd03752f0b58d8b82cd27916d1f2305bd
G4EVA-dev <tanzeglenn@gmail.com>
2024-07-10 15:28:25 +0100

    edit payment Success text

--- src/pages/paymentSuccess.js
+++ src/pages/paymentSuccess.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Confetti from "react-confetti";
-import { ReactComponent as TickIcon } from "../assets/images/tick.svg"; // Ensure you have a tick SVG
+import { ReactComponent as TickIcon } from "../assets/images/tick.svg";
 
 const PaymentSuccess = () => {
+
   return (
     <div className="w-full flex flex-col items-center justify-center min-h-screen bg-white">
       <Confetti />
       <TickIcon className="w-[156px] h-[156px] mb-4" />
       <h1 className="text-3xl font-bold mb-2">Payment Successful!</h1>
-      <p className="text-lg">Thank you for your purchase. Your transaction is complete.</p>
+      <p className="text-lg">Thank you for your purchase.</p>
     </div>
   );
 };

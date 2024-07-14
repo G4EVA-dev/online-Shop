ef72d39f79ba4e3d213623405a634279bcfc50c3
G4EVA-dev <tanzeglenn@gmail.com>
2024-07-14 17:12:44 +0100

    fixing addToCart

--- src/apiServices/timpuProductService.js
+++ src/apiServices/timpuProductService.js
@@ -1,4 +1,5 @@
-const BASE_URL = "https://api.timbu.cloud";
+// const BASE_URL = "https://api.timbu.cloud";
+const BASE_URL = "https://timbu-get-single-product.reavdev.workers.dev";
 const API_KEY = "0b05a82548ab4104bd5e007e7bfcacd620240712130521270028";
 const API_ID = "Y8ZE73OSI3STWFA";
 const ORGANISATION_ID = "3e60fab9119a4ceeb7363bfc44d629c0";
@@ -6,7 +7,12 @@ const ORGANISATION_ID = "3e60fab9119a4ceeb7363bfc44d629c0";
 export const fetchProductById = async (productId) => {
   try {
     const response = await fetch(
-      `${BASE_URL}/products/${productId}?Appid=${API_ID}&Apikey=${API_KEY}`,
+      // `${BASE_URL}/products/${productId}?Appid=${API_ID}&Apikey=${API_KEY}`
+
+      `${BASE_URL}/${productId}?organization_id=${ORGANISATION_ID}&Appid=${API_KEY}&Apikey=${API_ID}`,
+
+      // https://timbu-get-single-product.reavdev.workers.dev/e2fcb10e02334319857a4197c1d4c79b?organization_id=3e60fab9119a4ceeb7363bfc44d629c0&Appid=Y8ZE73OSI3STWFA&Apikey=0b05a82548ab4104bd5e007e7bfcacd620240712130521270028
+
       {
         method: "GET",
         headers: {
